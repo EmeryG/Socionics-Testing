b9c80d6212bd1a1bcd4f18b57fdaf1edab871b6c
EmeryG <Emerytgriffith@gmail.com>
2022-02-09 14:35:25 -0600

    Fix bug with negative question values

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var app = new Vue({
     el: "#app",
     data: {
-        q1: -1,
-        q2: -1,
+        q1: -100,
+        q2: -100,
         result_message: ''
     },
     methods: {
         submit_result: function() {
-            if(this.q1 > 0 && this.q2 > 0) {
+            if(this.q1 != -100 && this.q2 != -100) {
                 var result = this.q1 - this.q2;
                 if(result > 0) {
                     this.result_message = 'You like Socionics ' + Math.round(Math.abs(result)/4.0*100) + "%";
